0cd9f79149f9661428aecfb72bdf3414494f55f0
Elvin Ega <proega3@gmail.com>
2023-09-18 13:51:14 +0300

    forgot password

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -105,12 +105,90 @@ const LogIn = () => {
                           Remember this device
                         </label>
                       </div>
-                      <Link
-                        to="/dashboard"
+                      <button
+                        data-hs-overlay="#hs-modal-recover-account"
                         className="ml-auto text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
                       >
                         Lost Password?
-                      </Link>
+                      </button>
+                      <div
+                        id="hs-modal-recover-account"
+                        className="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto"
+                      >
+                        <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+                          <div className="bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-4 sm:p-7">
+                              <div className="text-center">
+                                <h2 className="block text-2xl font-bold text-gray-800 dark:text-gray-200">
+                                  Forgot password?
+                                </h2>
+                                <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                                  Remember your password?
+                                  <a
+                                    className="ml-3 text-blue-600 decoration-2 hover:underline font-medium"
+                                    href="/"
+                                  >
+                                    Sign in here
+                                  </a>
+                                </p>
+                              </div>
+                              <div className="mt-5">
+                                {/* Form */}
+                                <form>
+                                  <div className="grid gap-y-4">
+                                    {/* Form Group */}
+                                    <div>
+                                      <label
+                                        htmlFor="email"
+                                        className="block text-sm mb-2 dark:text-white"
+                                      >
+                                        Email address
+                                      </label>
+                                      <div className="relative">
+                                        <input
+                                          type="email"
+                                          id="email"
+                                          name="email"
+                                          className="py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                                          required=""
+                                          aria-describedby="email-error"
+                                        />
+                                        <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                                          <svg
+                                            className="h-5 w-5 text-red-500"
+                                            width={16}
+                                            height={16}
+                                            fill="currentColor"
+                                            viewBox="0 0 16 16"
+                                            aria-hidden="true"
+                                          >
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                          </svg>
+                                        </div>
+                                      </div>
+                                      <p
+                                        className="hidden text-xs text-red-600 mt-2"
+                                        id="email-error"
+                                      >
+                                        Please include a valid email address so
+                                        we can get back to you
+                                      </p>
+                                    </div>
+                                    {/* End Form Group */}
+                                    <button
+                                      type="submit"
+                                      className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+                                    >
+                                      Reset password
+                                    </button>
+                                  </div>
+                                </form>
+                                {/* End Form */}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                     <button className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                       Login to your account
