b6268a4e058d2ca92b4afd09a5ef625f5bb4e7ca
Elvin Ega <proega3@gmail.com>
2023-09-19 20:01:24 +0300

    add date selector

---
+++ src/components/DatePicker.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react"; // Replace 'your-datepicker-library' with the actual library you are using
+import DatePicker from "tailwind-datepicker-react";
+import { format } from "date-fns";
+
+interface DatePickerOptions {
+  title?: string;
+  autoHide?: boolean;
+  todayBtn?: boolean;
+  clearBtn?: boolean;
+  maxDate?: Date;
+  minDate?: Date;
+  theme?: {
+    disabledText?: string;
+  };
+  icons?: {
+    prev: () => JSX.Element;
+    next: () => JSX.Element;
+  };
+  datepickerClassNames?: string;
+  defaultDate?: Date;
+  language?: string;
+}
+const getFormattedTodayDate = (): string => {
+  const today = new Date();
+  return format(today, "yyyy-MM-dd"); // Format the date as "yyyy-MM-dd"
+};
+
+const options: DatePickerOptions = {
+  title: "Date",
+  autoHide: true,
+  todayBtn: false,
+  clearBtn: true,
+  maxDate: new Date("2030-01-01"),
+  minDate: new Date(getFormattedTodayDate()),
+  theme: {
+    disabledText: "bg-gray-100",
+  },
+  icons: {
+    prev: () => <span>Previous</span>,
+    next: () => <span>Next</span>,
+  },
+  datepickerClassNames: "top-12",
+  //   defaultDate: new Date("2022-01-01"),
+  language: "en",
+};
+
+const DatepickerComponent: React.FC = () => {
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleChange = (selectedDate: Date) => {
+    console.log(selectedDate);
+  };
+
+  const handleClose = (state: boolean) => {
+    setShow(state);
+  };
+
+  return (
+    <div>
+      {/* Replace 'Datepicker' with the actual component you are using */}
+      <DatePicker
+        options={options}
+        onChange={handleChange}
+        show={show}
+        setShow={handleClose}
+      />
+    </div>
+  );
+};
+
+export default DatepickerComponent;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import prelinePlugin from "preline/plugin";
 export default {
   content: [
     "node_modules/preline/dist/*.js",
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js", // <--- Add this line
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
