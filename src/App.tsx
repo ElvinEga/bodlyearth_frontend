57fb6d49901a4976ac833b1b6b7f5267736405c5
Elvin Ega <proega3@gmail.com>
2023-09-25 11:26:51 +0300

    createPassword Form

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,23 @@ import OtpPage from "./pages/login/otp";
 import Profile from "./pages/profile";
 import HelpPage from "./pages/help";
 import AdminHome from "./pages/home/adminhome";
+import CreatePassword from "./pages/login/createPassword";
 
 function App() {
   return (
     <>
       <Routes>
         {/* <Route path="/" element={<Dashboard />} exact /> */}
-        <Route path="/dashboard" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/engagement" element={<Engagement />} />
         <Route path="/team" element={<TeamMembers />} />
         <Route path="/roles" element={<Roles />} />
         <Route path="/companies" element={<Companies />} />
         <Route path="/recent_activity" element={<RecentActivity />} />
         <Route path="/resources" element={<Resources />} />
-        <Route path="/" element={<LogIn />} />
+        <Route path="/signin" element={<LogIn />} />
         <Route path="/verify" element={<OtpPage />} />
+        <Route path="/create_password" element={<CreatePassword />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/help" element={<HelpPage />} />
         <Route path="/admin" element={<AdminHome />} />
