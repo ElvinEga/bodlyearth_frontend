b359e01e84b84f5187057ba92dc98f4519f7bdc1
Elvin Ega <proega3@gmail.com>
2023-09-27 16:11:42 +0300

    axios configuration

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import BASE_URL from "./API_BASE_URL";
+
+export default axios.create({
+  baseURL: BASE_URL,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
