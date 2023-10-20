9850bf6b0eec23c8a1389d8282698ffff9dc8762
Elvin Ega <proega3@gmail.com>
2023-10-20 17:14:20 +0300

    remove error in maim

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "next-themes";
-import("preline");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
