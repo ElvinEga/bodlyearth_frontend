509749e6bdbaed27fc70d19d3900072e602bab05
Elvin Ega <proega3@gmail.com>
2023-10-30 15:25:38 +0300

    header title resize

--- src/components/breadheader.tsx
+++ src/components/breadheader.tsx
@@ -56,7 +56,7 @@ const BreadHeader = (props: Content) => {
         <h1 className="block text-2xl font-bold text-gray-800 sm:text-2xl dark:text-white">
           {props.title}
         </h1>
-        <p className="mt-2 text text-gray-800 dark:text-gray-400">
+        <p className="mt-1 text-sm text-gray-800 dark:text-gray-400">
           {props.description}
         </p>
       </header>
