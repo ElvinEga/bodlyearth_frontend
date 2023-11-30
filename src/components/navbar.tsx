894bf3f790dc0293d1901029dc2124fbf84122b1
Elvin Ega <proega3@gmail.com>
2023-11-30 14:44:09 +0300

    remove roles from navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -179,7 +179,7 @@ const Navbar = () => {
                   >
                     Users
                   </Link>
-                  <Link
+                  {/* <Link
                     className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-gray-300"
                     to="/roles"
                   >
@@ -190,7 +190,7 @@ const Navbar = () => {
                     to="/recent_activity"
                   >
                     Activity Logs
-                  </Link>
+                  </Link> */}
                 </div>
               </div>
               <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
