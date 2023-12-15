81a45bb78d53230b93b66b1e85616e949fae3e32
elvinega <proega3@gmail.com>
2023-12-15 19:12:19 +0300

    resize navcomponent

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
       {/* Sidebar */}
       <div
         id="application-sidebar"
-        className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-[22rem]  bg-white border-r border-gray-200  overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-gray-700"
+        className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-[28rem]  bg-white border-r border-gray-200  overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-gray-700"
       >
         <div className="flex h-full">
           <nav className="flex-none flex flex-col items-center text-center bg-teal-900 text-gray-400 border-r">
